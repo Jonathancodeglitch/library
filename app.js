27746c921e811df5fa5f87cd477b54332ec8a65d
Jonathan <jonathankendrick697@gmail.com>
2023-10-16 10:36:11 -0700

    removed dom functionalities from book class

--- app.js
+++ app.js
@@ -23,12 +23,44 @@ const modalController = (() => {
   };
 })();
 
+//controls the display of the app
 const domController = (() => {
-  
+  const createBookHtml = (book, index) => {
+    return `<div class="book" data-index="${index}" id="book">
+              <div class="item">Title : <span>${book.title}</span></div>
+              <div class="item">Author : <span>${book.author}</span></div>
+              <div class="item">Page : <span>${book.page} Pages</span></div>
+              <div class="button" id="read-status" style="background-color: ${
+                book.read ? '#A1CCD1' : 'rgb(241, 116, 116)'
+              }">${book.read ? 'Read' : 'Not Read'}</div>
+              <div class="button"  id="del-btn">REMOVE</div>
+          </div>`;
+  };
+
+  const createGreetingHtml = () => {
+    return `<div class="greeting">
+              Hey there scholar, your library is empty add a book!!
+            </div>`;
+  };
+
+  const renderBookHtml = (library) => {
+    const bookContainer = document.getElementById('library');
+    let bookhtml = library
+      .map((book, index) => createBookHtml(book, index))
+      .join('');
+    //
+    bookContainer.innerHTML =
+      library.length > 0 ? bookhtml : createGreetingHtml();
+  };
+
+  return {
+    renderBookHtml,
+  };
 })();
 
-/* encapsulate functions of book */
 
+
+/* encapsulate functions of book */
 class BookToLibrary {
   //store  books
   library = JSON.parse(localStorage.getItem('library')) || [];
@@ -40,88 +72,47 @@ class BookToLibrary {
     this.read = read;
   }
 
-  //catch Dom
-  bookContainer = document.getElementById('library');
-  modalForm = document.getElementById('modal-content');
-
-  //initialize
-  init() {
-    BookToLibrary.eventBinding();
-    this.render();
-  }
-
-  // create html for book
-  static createBookHtml(book, index) {
-    return `
-      <div class="book" data-index="${index}" id="book">
-        <div class="item">Title : <span>${book.title}</span></div>
-        <div class="item">Author : <span>${book.author}</span></div>
-        <div class="item">Page : <span>${book.page} Pages</span></div>
-        <div class="button" id="read-status" style="background-color: ${
-          book.read ? '#A1CCD1' : 'rgb(241, 116, 116)'
-        }">${book.read ? 'Read' : 'Not Read'}</div>
-        <div class="button"  id="del-btn">REMOVE</div>
-      </div>
-    `;
-  }
-
-  //create greeting html
-  static createGreetingHtml() {
-    return `<div class="greeting">
-              Hey there scholar, your library is empty add a book!!
-            </div>`;
-  }
-
-  // render all books currently in the #library array to the Dom
-  render() {
-    let bookhtml = this.library
-      .map((book, index) => BookToLibrary.createBookHtml(book, index))
-      .join('');
-
-    //check if library is empty and display the correct html
-    this.bookContainer.innerHTML =
-      this.library.length > 0 ? bookhtml : BookToLibrary.createGreetingHtml();
-  }
-
   //add book to #library array
-  addBookToLibrary = (e) => {
+  addBookToLibrary(e) {
     e.preventDefault();
     //inputs values
+    const modalForm = document.getElementById('modal-content');
     let title = document.getElementById('title').value;
     let author = document.getElementById('author').value;
     let page = document.getElementById('page').value;
     let read = document.getElementById('read').checked;
 
-    if (bookLibrary.modalForm.checkValidity()) {
+    if (modalForm.checkValidity()) {
       let newBook = new BookToLibrary(title, author, page, read);
+      console.log(newBook);
       this.library.push(newBook);
       modalController.closeModal();
       //reset modal input form
-      this.modalForm.reset();
+      modalForm.reset();
       //save current data to localStorage
       BookToLibrary.saveLibraryToLocalStorage();
       //update
-      this.render();
+      domController.renderBookHtml(this.library);
     }
-  };
+  }
 
   static saveLibraryToLocalStorage() {
     localStorage.setItem('library', JSON.stringify(bookLibrary.library));
   }
 
   //remove book from #librarys
-  removeBook = (e) => {
+  removeBook(e) {
     let target = e.target;
     if (target.id == 'del-btn') {
       let bookIndex = target.parentNode.dataset.index;
       this.library.splice(bookIndex, 1);
       BookToLibrary.saveLibraryToLocalStorage();
-      this.render();
+      domController.renderBookHtml(this.library);
     }
-  };
+  }
 
   //toggle read status
-  toggleReadStatus = (e) => {
+  toggleReadStatus(e) {
     let target = e.target;
     if (target.id == 'read-status') {
       let bookIndex = target.parentNode.dataset.index;
@@ -140,22 +131,31 @@ class BookToLibrary {
       }
 
       BookToLibrary.saveLibraryToLocalStorage();
-      this.render();
+      domController.renderBookHtml(this.library);
     }
-  };
+  }
 
   ///add eventListner to buttons
-  static eventBinding = () => {
-    bookLibrary.bookContainer.addEventListener('click', bookLibrary.removeBook);
-    bookLibrary.bookContainer.addEventListener(
-      'click',
-      bookLibrary.toggleReadStatus
-    );
-    bookLibrary.modalForm.addEventListener(
-      'submit',
-      bookLibrary.addBookToLibrary
-    );
-  };
+  static eventBinding() {
+    //catch Dom
+    const bookContainer = document.getElementById('library');
+    const modalForm = document.getElementById('modal-content');
+
+    bookContainer.addEventListener('click', (e) => {
+      bookLibrary.removeBook(e);
+      bookLibrary.toggleReadStatus(e);
+    });
+
+    modalForm.addEventListener('submit', (e) => {
+      bookLibrary.addBookToLibrary(e);
+    });
+  }
+
+  //initialize
+  init() {
+    BookToLibrary.eventBinding();
+    domController.renderBookHtml(this.library);
+  }
 }
 
 //class instance
